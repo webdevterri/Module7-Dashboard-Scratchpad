24f18d110039ef64de954633e2f78cefed37f5b5
Terri Jones <32005382+webdevterri@users.noreply.github.com>
2022-03-13 06:26:39 +0000

    Fixed box sizing & text shadow + added catch error

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.full})`
         imageAuthor.textContent = `by ${data.user.name} (${data.user.username})`
     })
+  .catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)`
+    imageAuthor.textContent = `by Jezael Melgoza (jezar)`
+  })
+  
