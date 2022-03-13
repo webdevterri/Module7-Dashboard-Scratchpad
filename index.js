69561091bbb136325811e7474bf7901a4c5c8e5b
Terri Jones <32005382+webdevterri@users.noreply.github.com>
2022-03-13 09:11:21 +0000

    Added the text "Photo" to photo credits to make it clear what the name refers to.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(data => {
         // console.log(data);
         document.body.style.backgroundImage = `url(${data.urls.full})`
-        imageAuthor.textContent = `by ${data.user.name} (${data.user.username})`
+        imageAuthor.textContent = `Photo by ${data.user.name} (${data.user.username})`
     })
   .catch(err => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)`
